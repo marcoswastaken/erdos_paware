67b5fc2da72a22f70a47a021a780a638c1b9a8bd
Marcos Ortiz <marcos.ortiz.math@gmail.com>
2024-04-13 14:41:23 -0600

    added scripts for processing labeled queries

---
+++ src/evaluating_results/process_labels.py
@@ -0,0 +1,99 @@
+import os
+import polars as pl
+
+humans = ["mo", "kk", "kp", "dr", "sr"]
+vote_cols = ["mo_label", "kk_label", "kp_label", "dr_label", "sr_label"]
+
+LABELED_DATA_DIR = "../data_labeling/labeled_results/"
+
+def get_merged_labels_and_votes(config:str)->pl.DataFrame:
+    """
+    Create a dataframes of merged labels for a given config
+
+    Args:
+        config (str): 
+            The configuration used to generate the query/result pairs 
+            for labeling.
+    
+    Returns:
+        pl.DataFrame: 
+            A dataframe with the merged labels for the given config.
+    """
+    ## Get filenames for labeled data in specified config
+    files = [
+        f for f in os.listdir("../data_labeling/labeled_results/") 
+        if f.startswith("labeled_"+config)]
+    
+    ## Read the first file to get the schema
+    df = pl.read_parquet(
+        LABELED_DATA_DIR+files[0]).sort(by=["query_text", "reddit_name"])
+
+    ## Merge all the files
+    for f in files[1:]:
+        ## Get the human name from the filename
+        human = f.split("_")[2][:2]
+
+        ## Read the file and sort by query_text and reddit_name
+        temp_df = pl.read_parquet(LABELED_DATA_DIR+f).sort(
+            by=["query_text", "reddit_name"])
+        
+        ## Update the labels in the main dataframe
+        df = df.with_columns(temp_df[human+"_label"].alias(human+"_label"))
+    
+    ## Add a column that has a list of all votes cast
+    df = df.with_columns(
+        pl.struct([pl.col(column_name) for column_name in vote_cols])
+        .map_elements(lambda s: [value for value in s.values() 
+                                 if value is not None])
+                                 .alias("votes"))
+    
+    return df
+
+def get_majority_vote(df:pl.DataFrame)->pl.DataFrame:
+    """
+    Get the majority vote for each query_text and reddit_name pair
+
+    Args:
+        df (pl.DataFrame): 
+            A dataframe with the merged labels for the given config.
+    
+    Returns:
+        pl.DataFrame: 
+            A dataframe with the majority vote for each query_text and 
+            reddit_name pair.
+    """
+    ## Set up the voting machine
+    def voting_machine(row: pl.Series) -> int:
+        votes = row.to_list()
+        votes_1 = votes.count(1)
+        votes_2 = votes.count(2)
+        votes_3 = votes.count(3)
+        
+        ## Clear winners
+        if (votes_1 > votes_2) & (votes_1 > votes_3):
+            return 1
+        elif (votes_2 > votes_1) & (votes_2 > votes_3):
+            return 2
+        elif (votes_3 > votes_2) & (votes_3 > votes_1):
+            return 3
+
+        ## Dealing with ties
+        if votes_1 == votes_2 > votes_3:
+            return 2
+
+        if votes_1 == votes_3 > votes_2:
+            return 3
+
+        if votes_2 == votes_3 > votes_1:
+            return 2
+
+        if votes_1 == votes_2 == votes_3:
+            return 2
+
+    # Apply the voting machine function
+    df = df.with_columns(
+        df['votes'].map_elements(
+            voting_machine, return_dtype=int).alias("relevance_rating")
+    )
+    
+    return df
